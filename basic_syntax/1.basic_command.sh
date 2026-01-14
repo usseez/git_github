04457ba63e36e74d48d26ea71e0bc3c325f044f0
usseez <dlawltn511@gmail.com>
2026-01-14 16:28:20 +0900

    java add

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -15,3 +15,15 @@ git commit -m "title" -m "내용"
 #vi editor mode 에서 작성 : 첫줄_제목, 두번째줄_내용
 git commit
 
+#add와 commit을 동시에 
+git commit -am "add와 commit을 동시에"
+
+#local repo의 commit 이력 조회
+git log
+
+git log --oneline  #간결조회
+git log --graph    # 그래프조회
+git log --all      # main브랜치 뿐만 아니라 전체 commit 이력 조회
+
+# 충돌 무시하고 강제로 push할 때
+git push origin main --force

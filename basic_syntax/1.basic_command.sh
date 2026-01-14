87ad7cd181f78c4e11a49ac7205a008b699d65c9
usseez <dlawltn511@gmail.com>
2026-01-14 11:01:50 +0900

    fix

---
+++ basic_syntax/1.basic_command.sh
@@ -0,0 +1,17 @@
+
+
+
+\
+
+# 현재 working directory, staging area의 상태 조회
+git status
+
+#특정 파일만 add할 경우
+git add 특정파일(위치포함)
+
+#local repository에 커밋 이력 생성
+git commit -m "title" -m "내용"
+
+#vi editor mode 에서 작성 : 첫줄_제목, 두번째줄_내용
+git commit
+

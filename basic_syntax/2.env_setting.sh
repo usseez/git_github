e69c0b3b2f524ba57a4000dba8324b2ac4743ffa
usseez <dlawltn511@gmail.com>
2026-01-14 09:33:17 +0900

    fix

---
+++ basic_syntax/2.env_setting.sh
@@ -0,0 +1,14 @@
+# 사용자 지정 방법
+# 전역적 사용자 이름,  email 지정
+git config --global user.name "유저네임"
+git config --global user.email "user@email"
+
+#지역적 사용자 지정
+git config --local user.name "유저네임"
+git config --local user.email "user@email"
+
+#사용자 정보 조회
+git config user.name
+git config user.email
+git config --list
+

5920e798b54fa24853e33135b81cda0219a3b9d7
usseez <dlawltn511@gmail.com>
2026-01-19 09:35:44 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -27,3 +27,11 @@ git log --all      # main브랜치 뿐만 아니라 전체 commit 이력 조회
 
 # 충돌 무시하고 강제로 push할 때
 git push origin main --force
+
+#특정 커밋 아이디로 전환(소스코드를 과거의 특정 버전으로 되돌릴때)
+git checkout [commit ID]
+ 
+#다시 현재의 브랜치 commit 되돌아올때
+git checkout 현재 브랜치명(main) 
+
+git pull
